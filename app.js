64a639b4ff6351d8561ceae80ae118681ed03d62
Vinícius Costa <vini.pc03@gmail.com>
2024-11-22 14:21:30 -0300

    Aula 9 express handlebars

--- app.js
+++ app.js
@@ -2,13 +2,37 @@
 
 const express = require('express');
 
+// importar módulo express-handlebars
+const { engine } = require('express-handlebars');
+
+// importar modulo do mysql
+const mysql = require('mysql2');
+
 // App
 const app = express();
 
-// Rota hello world
+// Configuração do express-handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+// configuração de conexão
+const conexao = mysql.createConnection({
+   host: 'localhost',
+   user:'root',
+   password: 'ifsp',
+   database:'projeto' 
+});
+
+// teste de conexão[
+conexao.connect(function(erro){
+    if(erro) throw erro;
+    console.log('Conexão efetuada com sucesso!');
+});
+
+// Rota principal
 app.get('/', function(req, res){
-    res.write('Hello World!');
-    res.end();
+    res.render('formulario');
 });
 
 // Servidor
